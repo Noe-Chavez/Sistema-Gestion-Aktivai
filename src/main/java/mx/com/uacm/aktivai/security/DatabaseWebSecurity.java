20a5136d04550bb14f746b641215c09f62841170
Noé Baldemar Chávez Morelos <noe_fam_chavez@yahoo.com.mx>
2022-10-09 15:45:46 -0500

    Configuración para especificar rutas públicas y privadas del sitio web.

--- src/main/java/mx/com/uacm/aktivai/security/DatabaseWebSecurity.java
+++ src/main/java/mx/com/uacm/aktivai/security/DatabaseWebSecurity.java
@@ -4,6 +4,7 @@ import mx.com.uacm.aktivai.utilities.QuerysSpringSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,4 +23,18 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery(QuerysSpringSecurity.queryParaRolesPorUsuario());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // Los recursos estáticos no requieren autenticación
+                .antMatchers(
+                        "/images/**"
+                ).permitAll()
+                // Las vistas públicas no requieren autenticación (Por el momento todas requieren autenticación)
+                .antMatchers().permitAll()
+                // Todas las demás URLs de la Aplicación requieren autenticación
+                .anyRequest().authenticated()
+                // El formulario de Login no requiere autenticacions
+                .and().formLogin().permitAll();
+    }
 }
